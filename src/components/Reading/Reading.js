2a3130299e79eda61df230c263964cd448797170
Alec <alec4nandes@gmail.com>
2023-10-26 16:40:22 -0700

    highlight current slide

--- src/components/Reading/Reading.js
+++ src/components/Reading/Reading.js
@@ -2,22 +2,33 @@ import "../../css/reading.css";
 import SingleCard from "./SingleCard/SingleCard";
 import Summary from "./Summary";
 
-function jumper(id) {
-    const elem = document.querySelector(`#${id}`);
-    elem.scrollIntoView();
-    elem.scrollTo({ top: 0 });
+function jumper(id, e) {
+    const slideElem = document.querySelector(`#${id}`);
+    slideElem.scrollIntoView();
+    slideElem.scrollTo({ top: 0 });
+    // highlight
+    const navElem = document.querySelector("nav"),
+        buttons = navElem.querySelectorAll("button"),
+        clickedButton = e.target;
+    buttons.forEach((button) => button.classList.remove("highlight"));
+    clickedButton.classList.add("highlight");
 }
 
 export default function Reading({ vibe, setVibe, question, cards }) {
     return (
         <div id="reading">
             <nav>
-                <button onClick={() => jumper(`slide-card-1`)}>1</button>
-                <button onClick={() => jumper(`slide-card-2`)}>2</button>
-                <button onClick={() => jumper(`slide-card-3`)}>3</button>
-                <button onClick={() => jumper(`slide-card-4`)}>4</button>
-                <button onClick={() => jumper(`slide-card-5`)}>5</button>
-                <button onClick={() => jumper(`slide-summary`)}>all</button>
+                <button
+                    onClick={(e) => jumper(`slide-card-1`, e)}
+                    className="highlight"
+                >
+                    1
+                </button>
+                <button onClick={(e) => jumper(`slide-card-2`, e)}>2</button>
+                <button onClick={(e) => jumper(`slide-card-3`, e)}>3</button>
+                <button onClick={(e) => jumper(`slide-card-4`, e)}>4</button>
+                <button onClick={(e) => jumper(`slide-card-5`, e)}>5</button>
+                <button onClick={(e) => jumper(`slide-summary`, e)}>all</button>
                 <button onClick={() => setVibe(null)}>new</button>
             </nav>
             <div id="slide-container">
