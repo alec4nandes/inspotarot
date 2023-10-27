c9651962a0ec908a45d43f0cdc047fafe8114ad1
Alec <alec4nandes@gmail.com>
2023-10-27 14:15:30 -0700

    remove target _blank from header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,18 @@ import { IS_DEVELOPMENT } from "../scripts/openai.js";
 import SignOut from "./SignOut.js";
 
 export default function Header({ hideSignOut }) {
-    return (
-        <header>
-            <h1>InspoTarot</h1>
-            <nav>
-                <Link subDir="" linkText="About" />
-                <Link subDir="privacy" linkText="Privacy Policy" />
-                <Link subDir="disclaimer" linkText="Disclaimer" />
-                {!hideSignOut && <SignOut />}
-            </nav>
-        </header>
-    );
-}
-
-function Link({ subDir, linkText }) {
     const getFirebaseHost = () =>
         IS_DEVELOPMENT ? "http://localhost:5002" : "";
 
     return (
-        <a
-            href={`${getFirebaseHost()}/${subDir}`}
-            target="_blank"
-            rel="noopener"
-        >
-            {linkText}
-        </a>
+        <header>
+            <h1>InspoTarot</h1>
+            <nav>
+                <a href={`${getFirebaseHost()}/`}>About</a>
+                <a href={`${getFirebaseHost()}/privacy`}>Privacy Policy</a>
+                <a href={`${getFirebaseHost()}/disclaimer`}>Disclaimer</a>
+                {!hideSignOut && <SignOut />}
+            </nav>
+        </header>
     );
 }
